807e0164e4efda35044bfa6738469c11b017f9ab
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2022-04-22 17:18:53 +0800

    Update transactedvariants.js

--- be-express/routes/transactedvariants.js
+++ be-express/routes/transactedvariants.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { body, matchedData, param } = require("express-validator");
 const { compareSync, hash, hashSync } = require("bcrypt");
-const transactedvariants = require("../models/transactedvariants");
+const transactedvariants = require("../models/transactedvariant");
 const {
   randomString,
   validateResultMiddleware,
@@ -10,77 +10,73 @@ const passport = require("passport");
 const upload = require("../libraries/multer");
 
 router.post(
-    "/add",
-    passport.authenticate("bearer", { session: false }),
-    [
-      body("variantid").notEmpty().isString(),
-      body("quantity").notEmpty().isString(),
-    ],
-    validateResultMiddleware,
+  "/add",
+  passport.authenticate("bearer", { session: false }),
+  [
+    body("variantid").notEmpty().isString(),
+    body("quantity").notEmpty().isString(),
+  ],
+  validateResultMiddleware,
 
-    async (req, res) => {
-      const { variantid, quantity } = matchedData(req, {
-        locations: ["body"],
+  async (req, res) => {
+    const { variantid, quantity } = matchedData(req, {
+      locations: ["body"],
+    });
+    try {
+      const newtransactedvariants = await transactedvariants.create({
+        variantid,
+        quantity,
       });
-      try {
-        const newtransactedvariants = await transactedvariants.create({
-          variantid,
-          quantity,
-        });
-  
-        res.send(newtransactedvariants);
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
-      }
-    }
-  );
-  router.delete(
-    "/",
-    passport.authenticate("bearer", { session: false }),
-    [
-      body("quantity").notEmpty().isString(),
-    ],
-    param("id").notEmpty().isNumeric(),
-    validateResultMiddleware,
-    async (req, res) => {
-      try {
-        await transactedvariants.destroy({
-          where: {
-            id: req.params.id,
-          },
-        });
-        res.send();
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: error.message });
-      }
-    }
-  );
-  router.post(
-    "/editquantity",
-    passport.authenticate("bearer", { session: false }),
-    [
-      body("quantity").notEmpty().isString(),
-    ],
-    param("id").notEmpty().isNumeric(),
-      validateResultMiddleware,
-  
-    async (req, res) => {
-      const { quantity } = matchedData(req, {
-        locations: ["body"],
-      });
-      try {
-        const newtransactedvariants = await transactedvariants.create({
-          quantity
-        });
-  
-        res.send(newtransactedvariants);
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
-      }
-    }
-  );
 
-  module.exports = router;
+      res.send(newtransactedvariants);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+router.delete(
+  "/",
+  passport.authenticate("bearer", { session: false }),
+  [body("quantity").notEmpty().isString()],
+  param("id").notEmpty().isNumeric(),
+  validateResultMiddleware,
+  async (req, res) => {
+    try {
+      await transactedvariants.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.send();
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error: error.message });
+    }
+  }
+);
+router.post(
+  "/editquantity",
+  passport.authenticate("bearer", { session: false }),
+  [body("quantity").notEmpty().isString()],
+  param("id").notEmpty().isNumeric(),
+  validateResultMiddleware,
+
+  async (req, res) => {
+    const { quantity } = matchedData(req, {
+      locations: ["body"],
+    });
+    try {
+      const newtransactedvariants = await transactedvariants.create({
+        quantity,
+      });
+
+      res.send(newtransactedvariants);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+module.exports = router;
