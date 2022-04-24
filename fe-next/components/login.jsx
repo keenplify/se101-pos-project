7e8cfa5bb67760f005e0cd07443d4f716bde5868
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2022-04-24 10:37:30 +0800

    Reverted username to User ID + fix
    Fixed login

--- fe-next/components/login.jsx
+++ fe-next/components/login.jsx
@@ -1,89 +1,121 @@
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import {MdDelete} from "react-icons/md"
-import {Card,Row, Col, Form, FormControl, Button, InputGroup, Table, Modal} from "react-bootstrap"
-import { BACKEND } from '../helpers';
-export default function LoginCard({employee}) {
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { MdDelete } from "react-icons/md";
+import {
+  Card,
+  Row,
+  Col,
+  Form,
+  FormControl,
+  Button,
+  InputGroup,
+  Table,
+  Modal,
+} from "react-bootstrap";
+import { BACKEND } from "../helpers";
+export default function LoginCard({ employee }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  
-    return (
-      <>
-      <br/>
-    <div>
-      <Card style={{
-      justifyContent:"center",
-      placeItems:"center",
-      marginTop:"60px",
-      width:"25vh",
-      padding:"8px 1px", 
-      border:"3px solid #DFE1E4",
-      borderRadius:"20px",
-      fontSize:"2.5vh",
-      fontWeight:"bold",
-      overflow:"auto",
-      boxShadow:"5px 5px 10px grey"
 
-      }}
-      onClick = {handleShow}>
-      {typeof employee.images !== undefined && employee?.images?.length>0 ? 
-    
-    
-    <Card.Img style={{
-      borderRadius:"50%",
-      width:"70%",
-      height:"70%",
-      border:"3px solid #DFE1E4"
-      }}variant="top" src={BACKEND + employee.images[0].location}/>:<span>Hello Aczell</span>}
- 
-      <Card.Body>
-        
-        <Card.Text className="text-center">
-      {employee.firstName} {employee.lastName}
-        </Card.Text>
-    
-      </Card.Body></Card></div>
-     
-    <Modal show={show} onHide={handleClose} aria-labelledby="contained-modal-title-vcenter"
-      centered>
+  return (
+    <>
+      <br />
+      <div>
+        <Card
+          style={{
+            justifyContent: "center",
+            placeItems: "center",
+            marginTop: "60px",
+            width: "25vh",
+            padding: "8px 1px",
+            border: "3px solid #DFE1E4",
+            borderRadius: "20px",
+            fontSize: "2.5vh",
+            fontWeight: "bold",
+            overflow: "auto",
+            boxShadow: "5px 5px 10px grey",
+          }}
+          onClick={handleShow}
+        >
+          {typeof employee.images !== undefined &&
+          employee?.images?.length > 0 ? (
+            <Card.Img
+              style={{
+                borderRadius: "50%",
+                width: "70%",
+                height: "70%",
+                border: "3px solid #DFE1E4",
+              }}
+              variant="top"
+              src={BACKEND + employee.images[0].location}
+            />
+          ) : (
+            <span>Hello Aczell</span>
+          )}
+
+          <Card.Body>
+            <Card.Text className="text-center">
+              {employee.firstName} {employee.lastName}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
         <Modal.Header closeButton>
-          <Modal.Title>You are about to log in as Genesis Admin</Modal.Title>
-          
+          <Modal.Title>
+            You are about to log in as {employee.firstName} {employee.lastName}
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body className="text-center">
-        <Form>
-
-  <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-    <Form.Label column sm="2">
-      <b>Password</b>
-    </Form.Label>
-    <Col sm="10">
-      <Form.Control type="password" placeholder="Password" />
-    </Col>
-  </Form.Group>
-</Form>
+          <Form>
+            <Form.Group
+              as={Row}
+              className="mb-3"
+              controlId="formPlaintextPassword"
+            >
+              <Form.Label column sm="2">
+                <b>Password</b>
+              </Form.Label>
+              <Col sm="10">
+                <Form.Control type="password" placeholder="Password" />
+              </Col>
+            </Form.Group>
+          </Form>
         </Modal.Body>
-       
+
         <Modal.Footer>
-          <Button style={{
-            borderRadius:"20px",
-            padding:"8px 14px",
-            fontWeight:"bold"
-          }}variant="secondary" onClick={handleClose}>
+          <Button
+            style={{
+              borderRadius: "20px",
+              padding: "8px 14px",
+              fontWeight: "bold",
+            }}
+            variant="secondary"
+            onClick={handleClose}
+          >
             Close
           </Button>
-         <Button style={{
-            borderRadius:"20px",
-            padding:"8px 14px",
-            fontWeight:"bold"
-          }}variant="primary" onClick={handleClose}>
+          <Button
+            style={{
+              borderRadius: "20px",
+              padding: "8px 14px",
+              fontWeight: "bold",
+            }}
+            variant="primary"
+            onClick={handleClose}
+          >
             Login
           </Button>
         </Modal.Footer>
       </Modal>
-      </>
-    );
-  }
-  
+    </>
+  );
+}
--- fe-next/pages/login.js
+++ fe-next/pages/login.js
@@ -6,115 +6,139 @@ import Router from "next/router";
 import { AuthenticateEmployee } from "../helpers/AuthenticateEmployee";
 import { useCookies } from "react-cookie";
 import { NavBar } from "../components/navbar";
-import {Card, Button, Container, Row, Col} from "react-bootstrap"
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import LoginCard from "../components/login";
 
-
-export default function Login({ employee,allEmployees}) {
+export default function Login({ employee, allEmployees }) {
   const [, setCookie] = useCookies(["token"]);
-  console.log(allEmployees)
+  console.log(allEmployees);
   return (
     <div>
       <NavBar employee={employee} />
       <Container>
-      <Row>
-        
-        <Col xs={12} md={7}>
-      <h1>Log in</h1>
-      
-      <label>{allEmployees && allEmployees.map(employee=><LoginCard employee={employee}></LoginCard>)}</label>
-      
-      
-      
-    </Col>
-    
+        <Row>
+          <Col xs={12} md={7}>
+            <h1>Log in</h1>
 
+            <label>
+              {allEmployees &&
+                allEmployees.map((employee) => (
+                  <LoginCard employee={employee}></LoginCard>
+                ))}
+            </label>
+          </Col>
 
-<Col xs={6} md={4}>
-      <Formik 
-
-        initialValues={{
-          id: "",
-          password: "",
-        }}
-        onSubmit={async (values, action) => {
-          axios
-            .post(BACKEND + "/api/employees/login", {
-              id: values.id,
-              password: values.password,
-            })
-            .then(function (response) {
-              setCookie("token", response.data.token);
-              Router.push("/");
-            })
-            .catch(function (error) {
-              console.log(error);
-              action.setStatus("Incorrect Username or Password ");
-            });
-        }}
-      >
-        {(formik) => (
-         <Container style={{
-           width:"60vh",
-           marginTop:"140px",
-           padding:"14px 16px", 
-           border:"3px solid #DFE1E4",
-           borderRadius:"20px",
-           fontWeight:"bold",
-           overflow:"auto",
-           boxShadow:"5px 5px 10px grey"}}><center>Vaperous Master</center>
-         <Form className style={{
-           margin:"auto", 
-           width:"70%",
-           padding:"10px"}}>
-            <label htmlFor="id">Username</label>
-            <Col>
-            <Field style={{
-              border:"1px solid #AEB1B4",
-              paddingLeft:"10px",
-              outline:"none",
-              width:"35vh"}} id="id" name="id" placeholder="Username"/>
-            </Col>
-            <label htmlFor="password">Password</label>
-            <Col>
-            <Field style={{
-              border:"1px solid #AEB1B4",
-              paddingLeft:"10px",
-              width:"35vh",
-              outline:"none"}}type="password" id="password"name="password" placeholder="Password"/>
-            </Col> 
-            <button style={{ 
-              backgroundColor:"#2274E1",
-              fontWeight:"bold",
-              color:"white",
-              borderRadius:"20px",
-              width:"35vh",
-              outline:"none",
-              border:"none",
-              padding:"8px 14px"}} 
-            variant ="primary" type="submit" className="my-3">Login
-            </button>
-           
-         </Form> {formik.status && <div style={{color:"red"}}>{formik.status}</div>}
-          </Container>
-        )}
-      </Formik>
-      </Col>
-      </Row>
+          <Col xs={6} md={4}>
+            <Formik
+              initialValues={{
+                id: "",
+                password: "",
+              }}
+              onSubmit={async (values, action) => {
+                axios
+                  .post(BACKEND + "/api/employees/login", {
+                    id: values.id,
+                    password: values.password,
+                  })
+                  .then(function (response) {
+                    setCookie("token", response.data.token);
+                    Router.push("/");
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                    action.setStatus("Incorrect Username or Password ");
+                  });
+              }}
+            >
+              {(formik) => (
+                <Container
+                  style={{
+                    width: "60vh",
+                    marginTop: "140px",
+                    padding: "14px 16px",
+                    border: "3px solid #DFE1E4",
+                    borderRadius: "20px",
+                    fontWeight: "bold",
+                    overflow: "auto",
+                    boxShadow: "5px 5px 10px grey",
+                  }}
+                >
+                  <center>Vaperous Master</center>
+                  <Form
+                    className
+                    style={{
+                      margin: "auto",
+                      width: "70%",
+                      padding: "10px",
+                    }}
+                  >
+                    <label htmlFor="id">User ID</label>
+                    <Col>
+                      <Field
+                        style={{
+                          border: "1px solid #AEB1B4",
+                          paddingLeft: "10px",
+                          outline: "none",
+                          width: "35vh",
+                        }}
+                        id="id"
+                        name="id"
+                        placeholder="User ID"
+                      />
+                    </Col>
+                    <label htmlFor="password">Password</label>
+                    <Col>
+                      <Field
+                        style={{
+                          border: "1px solid #AEB1B4",
+                          paddingLeft: "10px",
+                          width: "35vh",
+                          outline: "none",
+                        }}
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                      />
+                    </Col>
+                    <button
+                      style={{
+                        backgroundColor: "#2274E1",
+                        fontWeight: "bold",
+                        color: "white",
+                        borderRadius: "20px",
+                        width: "35vh",
+                        outline: "none",
+                        border: "none",
+                        padding: "8px 14px",
+                      }}
+                      variant="primary"
+                      type="submit"
+                      className="my-3"
+                    >
+                      Login
+                    </button>
+                  </Form>{" "}
+                  {formik.status && (
+                    <div style={{ color: "red" }}>{formik.status}</div>
+                  )}
+                </Container>
+              )}
+            </Formik>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
 }
 
-
 export async function getServerSideProps(context) {
-   const allEmployees=await axios.get(BACKEND + "/api/employees/all")
-  
-  return{
-    props: {
-      allEmployees:allEmployees.data.employees,
-      ...(await AuthenticateEmployee(context)).props
-    }
+  const allEmployees = await axios.get(BACKEND + "/api/employees/all");
 
-  }
+  return {
+    props: {
+      allEmployees: allEmployees.data.employees,
+      ...(await AuthenticateEmployee(context)).props,
+    },
+  };
 }
