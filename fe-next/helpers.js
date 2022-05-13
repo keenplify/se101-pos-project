369fc44a1baa505ce36e8a6fc8286c2ce362d91d
Aczell Bien Florencio <32163395+keenplify@users.noreply.github.com>
2022-05-13 23:51:35 +0800

    Updated backend link

--- fe-next/helpers.js
+++ fe-next/helpers.js
@@ -1,2 +1,3 @@
-export const BACKEND="http://localhost:5000"
+export const BACKEND="https://murmuring-savannah-02712.herokuapp.com"
+// "http://localhost:5000"
 export const COMPANY_NAME="VAPEROUS MASTER"
